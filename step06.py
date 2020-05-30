825055e6f5949133f281e3b58514965e92d1cbfd
keizi <keizi81@gmail.com>
2020-05-30 15:40:19 +0900

    step06

---
+++ step06.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+        self.grad = None
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        self.input = input
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backword(self, gy):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    def backword(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+
+    def backword(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+# 中心差分近似
+
+
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+b.grad = C.backword(y.grad)
+a.grad = B.backword(b.grad)
+x.grad = A.backword(a.grad)
+print(x.grad)
